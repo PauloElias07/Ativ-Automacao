e3a8f3500f248688285e0ca85b097dd0c632188e
Paulo Elias <paulo.elias@fatec.sp.gov.br>
2026-08-19 19:39:44 -0300

    Refactor test_divi and add zero division test

--- test_divi.py
+++ test_divi.py
@@ -1,4 +1,9 @@
-import pytest from app import divi
+import pytest
+from app import divi
 
 def test_divi():
     assert divi(10, 10) == 0
+
+def test_divi_zero():
+    with pytest.raises(ValueError):
+        divi(10, 0)
