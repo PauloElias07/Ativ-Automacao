2d3351914288a1fe03b2a8f264204d29328d2332
Paulo Elias <paulo.elias@fatec.sp.gov.br>
2026-09-23 10:27:01 -0300

    Refactor test_divi.py for readability and structure

--- test_divi.py
+++ test_divi.py
@@ -1,16 +1,19 @@
+import time
 import pytest
 from app import divi
 
+
 def test_divi():
     assert divi(10, 10) == 1
+
 
 def test_divi_zero():
     with pytest.raises(ValueError):
         divi(10, 0)
 
-while:
-    True:
-        test_divi()
-        test_divi_zero()
-        print("Testes passaram!")
-        time.sleep(1)
+
+while True:
+    test_divi()
+    test_divi_zero()
+    print("Testes passaram!")
+    time.sleep(1)
